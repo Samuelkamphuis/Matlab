32fed56bde55daabb749d1678c2da86946e293a5
Samuelkamphuis <skamphuis18@unm.edu>
2024-10-08 14:50:15 -0600

    Update app2.py

--- app2.py
+++ app2.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1j2q8VDmBv6u-FnonxQ7UMr4GRJoZ6_1J
 """
 
-! pip install streamlit
 
 import streamlit as st
 import pandas as pd
